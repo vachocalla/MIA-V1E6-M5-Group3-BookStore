6663b3c88a681b83a61b7a2d6333b9586acb289c
gaby21 <ggallardo@newvisiondata.com>
2026-07-25 18:05:50 -0400

    add table book_language

---
+++ BookStore-G3/Table/book_language.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[book_language]
+(
+	language_id int NOT NULL CONSTRAINT [PK_book_language] PRIMARY KEY,
+	language_code varchar(8) NULL,
+	language_name varchar(50) NULL,
+	[rowversion] timestamp NOT NULL
+)
