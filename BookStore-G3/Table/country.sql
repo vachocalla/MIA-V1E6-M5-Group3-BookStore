2276b1162c165b89f7d79d529d8cc2be17a04ebc
itsgaby <ggallardoff@gmail.com>
2026-07-25 19:50:34 -0400

    add remaining tables

---
+++ BookStore-G3/Table/country.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE country(
+	country_id int NOT NULL CONSTRAINT [PK_country] PRIMARY KEY,
+	country_name varchar(200) NULL,
+	[rowversion] timestamp NOT NULL
+);
---
+++ BookStore-G3/Table/customer_address.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE customer_address(
+	customer_id int NOT NULL,
+	address_id int NOT NULL,
+	status_id int NULL,
+	[rowversion] timestamp NOT NULL,
+	CONSTRAINT [PK_customer_address] PRIMARY KEY (customer_id, address_id)
+);
