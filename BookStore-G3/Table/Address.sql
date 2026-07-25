ac243d4dd229bf101f966395f6e4b8ac83306f9d
michaellimpias-ciber <michael.limpias@gmail.com>
2026-07-25 18:35:05 -0400

    Agregando Tabla Address

---
+++ BookStore-G3/Table/Address.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Address]
+(
+	[Id] INT NOT NULL CONSTRAINT PK_address PRIMARY KEY,
+	[address_id] [int] NOT NULL,
+	[street_number] [varchar](10) NULL,
+	[street_name] [varchar](200) NULL,
+	[city] [varchar](100) NULL,
+	[country_id] [int] NULL,
+	[rowversion] [timestamp] NOT NULL,
+ );
