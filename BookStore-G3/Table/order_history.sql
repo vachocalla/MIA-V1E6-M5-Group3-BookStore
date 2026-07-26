4d11b2ace1a098aea2d98586033b534233fcc1d6
VAchocalla <vachocalla@gmail.com>
2026-07-25 20:10:16 -0400

    Se agrega tablas Victor - Add Gitignore

---
+++ BookStore-G3/Table/order_history.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE order_history(
+	history_id int NOT NULL CONSTRAINT [PK_order_history] PRIMARY KEY,
+	order_id int NULL,
+	status_id int NULL,
+	status_date datetime NULL,
+	[rowversion] timestamp NOT NULL
+);
---
+++ BookStore-G3/Table/order_status.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE order_status(
+	status_id int NOT NULL CONSTRAINT [PK_order_status] PRIMARY KEY,
+	status_value varchar(20) NULL,
+	[rowversion] timestamp NOT NULL
+);
