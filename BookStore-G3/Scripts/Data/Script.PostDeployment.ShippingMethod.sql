95631e1c98e8ac7360f3dd0361115067f3bec83e
VAchocalla <vachocalla@gmail.com>
2026-08-05 04:44:36 -0400

    Ajustes adicionales tabla OrderLine

---
+++ BookStore-G3/Scripts/Data/Script.PostDeployment.ShippingMethod.sql
@@ -0,0 +1,17 @@
+﻿MERGE INTO shipping_method AS Target
+USING (VALUES 
+    (1,'Standard',5.90),
+    (2,'Priority',8.90),
+    (3,'Express',11.90),
+    (4,'International',24.50)
+) AS Source (method_id, method_name, cost)
+ON (Target.method_id = Source.method_id)
+
+WHEN MATCHED THEN 
+    UPDATE SET 
+        Target.method_name = Source.method_name,
+        Target.cost = Source.cost
+    
+WHEN NOT MATCHED BY TARGET THEN 
+    INSERT (method_id, method_name, cost) 
+    VALUES (Source.method_id, Source.method_name, Source.cost);
---
+++ BookStore-G3/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,40 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+:r .\Data\Script.PostDeployment.Author.sql
+
+:r .\Data\Script.PostDeployment.Publisher.sql
+
+:r .\Data\Script.PostDeployment.BookLanguage.sql
+
+:r .\Data\Script.PostDeployment.Book.sql
+
+:r .\Data\Script.PostDeployment.BookAuthor.sql
+
+:r .\Data\Script.PostDeployment.ShippingMethod.sql
+
+:r .\Data\Script.PostDeployment.OrderStatus.sql
+
+:r .\Data\Script.PostDeployment.Country.sql
+
+:r .\Data\Script.PostDeployment.AddressStatus.sql
+
+:r .\Data\Script.PostDeployment.Customer.sql
+
+:r .\Data\Script.PostDeployment.Address.sql
+
+:r .\Data\Script.PostDeployment.CustomerAddress.sql
+
+:r .\Data\Script.PostDeployment.CusOrder.sql
+
+:r .\Data\Script.PostDeployment.OrderHistory.sql
+
+:r .\Data\Script.PostDeployment.OrderLine.sql
