3121bcff40f28ed7e76b7b03430b056158220ca2
michaellimpias-ciber <michael.limpias@gmail.com>
2026-08-05 23:18:50 -0400

    Creación de Tabla de Hechos Fact_Order_Details

--- BookStoreBI/BookStoreDW/BookStore-G3-Dw/Staging/Shipping.sql
+++ BookStoreBI/BookStoreDW/BookStore-G3-Dw/Staging/Shipping.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [Staging].[ShippingMethod]
+﻿CREATE TABLE [Staging].[Shipping]
 (
 	[shipping_key] [int] NOT NULL,
 	[method_name] [varchar](100) NULL,
--- BookStoreBI/BookStoreDW/BookStore-G3-Dw/Table/Dim_Shipping.sql
+++ BookStoreBI/BookStoreDW/BookStore-G3-Dw/Table/Dim_Shipping.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[Dim_ShippingMethod]
+﻿CREATE TABLE [dbo].[Dim_Shipping]
 (
-	[shipping_key] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Dim_ShippingMethod] PRIMARY KEY CLUSTERED,
+	[shipping_key] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Dim_Shipping] PRIMARY KEY CLUSTERED,
 	[method_id] [int] NOT NULL,
 	[method_name] [varchar](100) NULL,
 	[cost] [decimal](10, 2) NULL,
